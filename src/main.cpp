4b5d43c76d2dcbdf593b06e8efcc02e09f5f9727
Jose Antonio de la Fuente Sanz <delafu@MacBook-Air-de-Jose.local>
2017-12-12 23:44:10 +0100

    main yellow line solved

--- src/main.cpp
+++ src/main.cpp
@@ -103,7 +103,7 @@ int main() {
             double shift_x = ptsx[i] - px;
             double shift_y = ptsy[i] - py;
             ptsx[i] = (shift_x * cos(0 - psi) - shift_y * sin(0-psi));
-            ptsy[i] = (shift_x * sin(0 - psi) - shift_y * cos(0-psi));            
+            ptsy[i] = (shift_x * sin(0 - psi) + shift_y * cos(0-psi));            
           }
 
           double* ptrx = &ptsx[0];
